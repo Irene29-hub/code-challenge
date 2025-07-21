39fb59d06cfb2bc06d60cb051789dd289b135d9f
irene29-hub <muemarenee@gmail.com>
2025-07-21 09:13:36 +0300

    chore: remove unused CORS middleware file

--- cors-middleware.js
+++
@@ -1,6 +0,0 @@
-export default (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-  next();
-}
